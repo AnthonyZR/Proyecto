66890054084d5245d16d7e9232a8f1d5cae51dfb
AnthonyZR <amme.zr.1005@gmail.com>
2026-06-06 10:18:56 -0500

    prdts

---
+++ ProyectoTM/JS/productos.js
@@ -0,0 +1,37 @@
+const productos = [
+    {id: "prod-01",
+    nombre: "Silla ErgoMax",
+    precio: 159,
+    categoria: "oficina",
+    imagen: "../Img/silla de oficina.png"},
+
+    {id: "prod-02",
+    nombre: "Cómoda Sable 4",
+    precio: 89,
+    categoria: "dormitorio",
+    imagen: "../Img/comoda.png"},
+
+    {id: "prod-03",
+    nombre: "Espejo Aura Oval",
+    precio: 99,
+    categoria: "decoracion",
+    imagen: "../Img/espejo aura oval.png"},
+
+    {id: "prod-04",
+    nombre: "Sofá Atlas",
+    precio: 699,
+    categoria: "sala",
+    imagen: "../Img/sofa.jpg"},
+
+    {id: "prod-05",
+    nombre: "Mesa Nórdica",
+    precio: 129,
+    categoria: "comedor",
+    imagen: "../Img/mesa centro nordica.png"},
+
+    {id: "prod-06",
+    nombre: "Cama Tapizada Oslo",
+    precio: 389,
+    categoria: "dormitorio",
+    imagen: "../Img/cama tapizada oslo.png"},
+];
